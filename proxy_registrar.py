3ba414f5dfbc7c4f03fdaeee2fe721eeea11fd3b
Eva Maria Perez Fernandez <evam@MacBook-Pro-de-EvaM.local>
2017-01-04 23:00:40 +0100

    4 Enero

--- proxy_registrar.py
+++ proxy_registrar.py
@@ -6,41 +6,27 @@ import time
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
-CONFIG = sys.argv [1]
-
-if len(sys.argv) != 2:
-    sys.exit("Usage: python proxy_registrar.py config")
 
 class XMLHandler(ContentHandler):   
 
-	def __init__(self):
-	    self.lista = []
-	    self.DicEtiquetas = {
-	        'server':['name','ip','data'],
-	        'database':['path','passwdpath'],
-	        'log':['path']}
+    def __init__(self):
+        self.lista = []
+        self.DicEtiquetas = {
+            'server':['name','ip','data'],
+            'database':['path','passwdpath'],
+            'log':['path']}
 
-	def startElement(self, name, attrs):
-	    if name in self.DicEtiquetas:
-	        dicc = {}
-	        for attrs in self.DicEtiquetas[name]:
-	            dicc[attrs] = attrs.get(attrs,"")
-            diccname = {name: dicc}
+    def startElement(self, name, attrs):
+        if name in self.DicEtiquetas:
+            dicc = {}
+            for name in self.DicEtiquetas[name]:
+                dicc[name] = attrs.get(name,"")
+            diccname = {name : dicc}
             self.lista.append(diccname)
 
     def get_tags(self):
-        return self.lista            
-	        
+        return self.lista     
 
-
-
-if __name__ == "__main__":
-    serv = socketserver.UDPServer((sys.argv[1], ContentHandler))
-    print("Lanzando servidor UDP de eco...")
-    try:
-        serv.serve_forever()
-    except KeyboardInterrupt:
-        print("Finalizado servidor")
 
 class SIPProxyRegisterHandler(socketserver.DatagramRequestHandler):
 
@@ -54,6 +40,8 @@ class SIPProxyRegisterHandler(socketserver.DatagramRequestHandler):
             data_text = text.decode('utf-8')
             Words_datas = data_text.split()
             REQUEST = Words_datas[0]
+            Dir_IP = self.client_address[0]
+            Puerto = self.client_address[1]
             print("La peticion es: ", REQUEST)
             print("Listening...")
             if not line:
@@ -62,4 +50,23 @@ class SIPProxyRegisterHandler(socketserver.DatagramRequestHandler):
             if REQUEST == "register":
                 Message = ("REGISTER sip:" + DIRECCTION + " SIP/2.0\r\n")
                 Message += ("Expires: " + EXPIRES + "\r\n\r\n")
+                user = data_text[1].split(':')[1]
                 print("Enviando:", Message)
+
+if __name__ == "__main__":
+    
+    CONFIG = sys.argv [1]
+    if len(sys.argv) != 2:
+        sys.exit("Usage: python proxy_registrar.py config")
+    
+    parser = make_parser()
+    cHandler = XMLHandler()
+    parser.setContentHandler(cHandler)
+    parser.parse(open(CONFIG))
+    config = cHandler.get_config()
+    serv = socketserver.UDPServer(("",int(config[2])),SIPProxyRegisterHandler)
+    print("Lanzando servidor UDP de eco...")
+    try:
+        serv.serve_forever()
+    except KeyboardInterrupt:
+        print("Finalizado servidor")
